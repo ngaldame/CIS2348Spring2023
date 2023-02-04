1868ea2eb939b30c1aecf58fec830325a1f4308a
ngaldame <ngaldame@cougarnet.uh.edu>
2023-02-03 20:37:31 -0600

    One line added

--- Homework1/ZyLab_1_20.py
+++ Homework1/ZyLab_1_20.py
@@ -1,1 +1,9 @@
-user_num = int(input('Please enter an integer: ')
+user_num = int(input('Please enter an integer:\n'))
+print('You entered:', user_num)
+# Square then cube user_num
+print(user_num, 'squared is', (user_num ** 2))
+print('And', user_num, 'cubed is', (user_num ** 3), '!!')
+# Get second input which will be called user_num2
+user_num2 = int(input('Please enter another integer:\n'))
+print(user_num, '+', user_num2, 'is', (user_num + user_num2))
+print(user_num, '*', user_num2, 'is', (user_num * user_num2))
