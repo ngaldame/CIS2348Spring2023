0d92222e43fa3024f6778e4b530e093049bf4e94
ngaldame <ngaldame@cougarnet.uh.edu>
2023-02-04 14:23:19 -0600

    Do non-birthday code later

--- Homework1/Code_Problem_01.py
+++ Homework1/Code_Problem_01.py
@@ -1,3 +1,19 @@
 # Nathan Galdamez Gomez
 
 print('Birthday Calculator\n')
+print('Enter the current date in this order: month, day, and year.')
+current_month = int(input('Current Month: '))
+current_day = int(input('Current Day: '))
+current_year = int(input('Current Year: '))
+print('Enter your birthday in this order: month, day, and year.')
+birth_month = int(input('Birth Month: '))
+birth_day = int(input('Birth Day: '))
+birth_year = int(input('Birth Year: '))
+
+yearAge = current_year - birth_year
+monthAge = current_month - birth_month
+dayAge = current_day - birth_day
+
+if current_month == birth_month and current_day == birth_day:
+    print('Happy Birthday!')
+# Do non-birthday code later
