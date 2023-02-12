4ba31780ce9d79a21b8a59c372f43349011b9614
ngaldame <ngaldame@cougarnet.uh.edu>
2023-02-12 16:34:58 -0600

    One line added

--- Homework1/Code_Problem_01.py
+++ Homework1/Code_Problem_01.py
@@ -1,19 +1,28 @@
-# Nathan Galdamez Gomez
+# Nathan Galdamez Gomez, ngaldame
 
-print('Birthday Calculator\n')
-print('Enter the current date in this order: month, day, and year.')
-current_month = int(input('Current Month: '))
-current_day = int(input('Current Day: '))
-current_year = int(input('Current Year: '))
-print('Enter your birthday in this order: month, day, and year.')
-birth_month = int(input('Birth Month: '))
-birth_day = int(input('Birth Day: '))
-birth_year = int(input('Birth Year: '))
+print('\nBirthday Calculator\n')
+print('Current day')
+current_month = int(input('Month: '))
+current_day = int(input('Day: '))
+current_year = int(input('Year: '))
+print('Birthday')
+birth_month = int(input('Month: '))
+birth_day = int(input('Day: '))
+birth_year = int(input('Year: '))
 
+# Create variables to get a user's age
 yearAge = current_year - birth_year
 monthAge = current_month - birth_month
 dayAge = current_day - birth_day
 
+# If statement prints 'Happy Birthday!' and increment yearAge by 1
 if current_month == birth_month and current_day == birth_day:
     print('Happy Birthday!')
-# Do non-birthday code later
+    yearAge += 1
+# elif statement will print age before birthday
+elif current_month <= birth_month and current_day < birth_day:
+    yearAge -= 1
+    print(f'You are {yearAge} years old.')
+# elif statement will print current age
+elif current_month >= birth_month and current_day >= birth_day:
+    print(f'You are {yearAge} years old.')
