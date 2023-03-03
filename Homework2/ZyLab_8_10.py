6c6c37db455aad3d6485eb34ca9e4e142060a2cd
ngaldame <ngaldame@cougarnet.uh.edu>
2023-03-03 04:03:57 -0600

    One line added

--- Homework2/ZyLab_8_10.py
+++ Homework2/ZyLab_8_10.py
@@ -1,4 +1,15 @@
 # Nathan Galdamez Gomez, 2141118
+def palindrome(a):
+    b = a.replace(' ', '')
+    return b == b[::-1]
 
+
+# Get input and store input in the drome variable that will call the palindrome function for the input
 word = str(input())
 drome = palindrome(word)
+
+# Test if input is a palindrome or not
+if drome:
+    print(f'{word} is a palindrome')
+else:
+    print(f'{word} is not a palindrome')
