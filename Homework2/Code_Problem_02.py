69f6bf3f59bdd87675ae861564bfff767f0afb0c
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-08 03:58:26 -0600

    Delete Code_Problem_02.py

--- Homework2/Code_Problem_02.py
+++
@@ -1,8 +0,0 @@
-# Nathan Galdamez Gomez, 2141118
-# This is Coding Problem 2
-
-# The below code is for Part A
-
-# Use import datetime module and create an empty array to store input dates
-from datetime import datetime
-dates = []
