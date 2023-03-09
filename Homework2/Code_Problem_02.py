829c06616a42b70947a59f2f44cb3cff634c8048
ngaldame <ngaldame@cougarnet.uh.edu>
2023-03-08 22:57:30 -0600

    This is Part C for writing to an output file

--- Homework2/Code_Problem_02.py
+++ Homework2/Code_Problem_02.py
@@ -1,7 +1,7 @@
 # Nathan Galdamez Gomez, 2141118
 # This is Coding Problem 2
 
-# The below code is for Part B
+# The below code is for Part C
 
 # Use import datetime module
 from datetime import datetime
@@ -10,21 +10,24 @@ from datetime import datetime
 # dates = []
 
 # Read all dates from inputDates.txt in order to read every single line
+# Then write correctly formatted dates into parsedDates.txt
 with open('inputDates.txt', 'r') as file:
-    for single_line in file:
-        # Remove whitespace in each line
-        date = single_line.strip()
-        if date == '-1':
-            break
-        # Try and except statement since if-elif-else statements gave me errors for wrong input
-        try:
-            # Either convert input date to the correct format OR output an error message
-            date_holder = datetime.strptime(date, '%B %d, %Y')
-            today = datetime.now()
+    with open('parsedDates.txt', 'w') as output:
+        for single_line in file:
+            # Remove whitespace in each line and break if date == -1
+            date = single_line.strip()
+            if date == '-1':
+                break
 
-        # Print if today is the greater value
-        # I used # for removing leading 0s on Windows
-            if date_holder <= today:
-                print(date_holder.strftime('%#m/%#d/%Y'))
-        except ValueError:
-            pass
+            # Try and except statement since if-elif-else statements gave me errors for wrong input
+            try:
+                # Either convert input date to the correct format OR output an error message
+                date_holder = datetime.strptime(date, '%B %d, %Y')
+                today = datetime.now()
+
+            # Print if today is the equal or greater value
+            # I used # for removing leading 0s on Windows, - is for other OSs
+                if date_holder <= today:
+                    output.write(date_holder.strftime('%#m/%#d/%Y') + '\n')
+            except ValueError:
+                pass
