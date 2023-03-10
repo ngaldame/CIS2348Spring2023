3a3a3b1732cd5a573f52c0c96747ae8ffa763747
ngaldame <ngaldame@cougarnet.uh.edu>
2023-03-10 17:54:11 -0600

    Inputs added and calculated win percentage

--- Homework3/ZyLab_10_15.py
+++ Homework3/ZyLab_10_15.py
@@ -1,23 +1,33 @@
 # Nathan Galdamez Gomez, 2141118
+# This is ZyLab 10.15
 
 # This is a class called Team
 class Team:
     def __init__(self):
-        self.teamname = 'none'
+        self.teamname = 'None'
         self.team_wins = 0
         self.team_losses = 0
-        
-    # TODO: Define get_win_percentage()
+
+    # Define get_win_percentage function
     def get_win_percentage(self):
-        return team_wins / (team_wins + team_losses)
-            
+        return self.team_wins / (self.team_wins + self.team_losses)
+
+
 # This is the driver code
 if __name__ == '__main__':
     # Create an object of the Team class
     team = Team()
-    # Get inputs for team name, wins, and losses
+    # Get inputs for team name, team wins, and team losses
     name = str(input())
-    wins = float(input())
-    losses = float(input())
-    # TODO: Call get_win_percentage function and print win percentage
-    
+    wins = int(input())
+    losses = int(input())
+    # Call get_win_percentage function and print win percentage
+    team.teamname = name
+    team.team_wins = wins
+    team.team_losses = losses
+
+    # Print win percentage
+    if team.get_win_percentage() > 0.5:
+        print(f'Congratulations, Team {name} has a winning average!')
+    else:
+        print(f'Team {name} has a losing average.')
