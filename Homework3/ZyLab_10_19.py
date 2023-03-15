ea62398037c600f6ba8125684bbb7f59260c7c52
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-15 05:00:42 -0400

    Made ShoppingCart class with attributes & methods

--- Homework3/ZyLab_10_19.py
+++ Homework3/ZyLab_10_19.py
@@ -21,7 +21,73 @@ class ItemToPurchase:
     def print_item_description(self):
         print(f'{self.item_name}: {self.item_description}')
 
-
+# A class called ShoppingCart
+class ShoppingCart:
+    def __init__(self, customer_name = 'none', current_date = 'January 1, 2016', cart_items = []):
+        self.customer_name = customer_name
+        self.current_date = current_date
+        self.cart_items = cart_items
+    
+    # Add methods for ShoppingCart class
+    def add_item(self, itemToPurchase):
+        self.cart_items.append(itemToPurchase)
+    def remove_item(self, s):
+        r_item = False
+        for i in self.cart_items:
+            if i.item_name == s:
+                self.cart_items.remove(i)
+                r_item = True
+                break
+            # Maybe change else statement to if not depending on output
+            else:
+                print('Item not found in cart. Nothing removed.')
+    # Modify item ONLY if item being purchased is the same as an item in cart_items
+    def modify_item(self, itemToPurchase):
+        m_item = False
+        for i in range(len(self.cart_items)):
+            if self.cart_items[i].item_name == itemToPurchase.item_name:
+                m_item = True
+                self.cart_items[i].item_quantity = itemToPurchase.item_quantity
+                break
+            else:
+                print('Item not found in cart. Nothing modified.')
+    # Just return number of items in cart
+    def get_num_items_in_cart(self):
+        item_num = 0
+        for i in self.cart_items:
+            item_num = item_num + i.item_quantity
+        return item_num
+    # Get the final cost of items in cart
+    def get_cost_of_items(self):
+        initial_cost = 0
+        total_cost = 0
+        for i in self.cart_items:
+            initial_cost = i.item_quantity * i.item_price
+            total_cost += initial_cost
+        return total_cost
+    # Print total cost
+    def print_total(self):
+        total_cost = self.get_cost_of_items()
+        # if-else statement will find if total_cost is either 0 or not
+        if total_cost != 0:
+            print(f'{self.customer_name}\'s Shopping Cart - {self.current_date}')
+            print(f'Number of Items: {self.get_num_items_in_cart}')
+            for i in self.cart_items:
+                i.print_item_cost()
+            print(f'\nTotal: {total_cost}')
+        else:
+            print('SHOPPING CART IS EMPTY')
+    # Print descriptions of each item
+    def print_descriptions(self):
+        # if-else statement will determine whether there are items in cart
+        if len(self.cart_items) != 0:
+            print(f'{self.customer_name}\'s Shopping Cart - {self.current_date}\n')
+            print('Item Descriptions')
+            for item in self.cart_items:
+                item.print_item_description()
+        else:
+            print('SHOPPING CART IS EMPTY')
+            
 # Below is the main driver code
 if __name__ == '__main__':
     # Create 2 objects of ItemToPurchase class and get input of both item's attributes
@@ -43,3 +109,4 @@ if __name__ == '__main__':
     two.print_item_cost()
     print(f'\nTotal: ', end='')
     print(f'${math.floor(one.item_price) * one.item_quantity + math.floor(two.item_price) * two.item_quantity}')
+    
