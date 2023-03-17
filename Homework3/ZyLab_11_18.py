a2f176078b94dcefc04c4c4cd297c000f3d79657
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-17 18:00:50 -0500

    Made variables and a list for input of numbers

---
+++ Homework3/ZyLab_11_18.py
@@ -0,0 +1,8 @@
+# Nathan Galdamez Gomez, 2141118
+# This is ZyLab 11.18
+
+# Get input for nums, then split nums into a variable named ns
+nums = input()
+ns = nums.split()
+# Make an empty array called num_list
+num_list = []
