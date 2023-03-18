d1181f2f99d63776bb3e7efa119d4c793be0ed98
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-18 14:40:09 -0500

    Search and sort each number in num_list then print

--- Homework3/ZyLab_11_18.py
+++ Homework3/ZyLab_11_18.py
@@ -6,3 +6,16 @@ nums = input()
 ns = nums.split()
 # Make an empty array called num_list
 num_list = []
+
+# for loop to search each number in ns
+for n in ns:
+    # if statement will append n in num_list if n > 0
+    if int(n) > 0:
+        num_list.append(int(n))
+
+# Sort the list
+num_list.sort()
+
+# print each number in num_list on one single line
+for num in num_list:
+    print(num, end=' ')
