4158d42911b707a2d8dce1920e3a011491f18688
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-23 19:02:41 -0400

    Get input of words in a list and split that input

---
+++ Homework3/ZyLab_11_22.py
@@ -0,0 +1,8 @@
+# Nathan Galdamez Gomez, 2141118
+# This is ZyLab 11.22
+
+# words variable will take string input and split the input of words into a variable called ws
+words = str(input())
+ws = words.split()
+
+# TODO: count the frequency of each word in ws
