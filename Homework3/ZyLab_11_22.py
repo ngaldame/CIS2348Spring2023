e1709b618bf973df59a3b5cb69cfb1e06e4ed59e
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-03-24 04:24:57 -0400

    Print words and frequencies of each word

--- Homework3/ZyLab_11_22.py
+++ Homework3/ZyLab_11_22.py
@@ -1,8 +1,15 @@
 # Nathan Galdamez Gomez, 2141118
 # This is ZyLab 11.22
 
-# words variable will take string input and split the input of words into a variable called ws
+# words variable takes string input and splits inputted words into a variable called ws
 words = str(input())
 ws = words.split()
 
-# TODO: count the frequency of each word in ws
+# The frequency of each word in ws will be counted in for loop
+for w in ws:
+    
+    # freq is a variable that will hold each word's frequency
+    freq = ws.count(w)
+    
+    #Print the word and its frequenccy
+    print(w, freq)
