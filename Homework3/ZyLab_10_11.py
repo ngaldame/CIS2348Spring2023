6f62c66f7410e78ef0ccf5ce40302807c881f808
ngaldame <122861835+ngaldame@users.noreply.github.com>
2023-04-01 03:31:36 -0500

    A class called FoodItem is implemented

---
+++ Homework3/ZyLab_10_11.py
@@ -0,0 +1,23 @@
+# Nathan Galdamez Gomez, 2141118
+
+# This is the class FoodItem
+class FoodItem:
+    def __init__(self, name='None', fat=0.0, carbs=0.0, protein=0.0):
+        self.name = name
+        self.fat = fat
+        self.carbs = carbs
+        self.protein = protein
+
+    # This function will have servings passed into its' constructor
+    def get_calories(self, num_servings):
+        # Calorie formula
+        calories = ((self.fat * 9) + (self.carbs * 4) + (self.protein * 4)) * num_servings;
+        return calories
+
+    # This function will print the name and its' instance attributes
+    def print_info(self):
+        print('Nutritional information per serving of {}:'.format(self.name))
+        print('   Fat: {:.2f} g'.format(self.fat))
+        print('   Carbohydrates: {:.2f} g'.format(self.carbs))
+        print('   Protein: {:.2f} g'.format(self.protein))
+
