17692dc5391d74325d1a2612c94e9b24ffed7d91
ngaldame <ngaldame@cougarnet.uh.edu>
2023-04-28 04:31:31 -0500

    Printed average of the list's elements with 2 digits after the decimal

--- ExtraCredit/ZyLab_11_25.py
+++ ExtraCredit/ZyLab_11_25.py
@@ -9,4 +9,9 @@ weight_4 = float(input('Enter weight 4:\n'))
 
 # All weights will be added to a list called all_weights, then will be outputted
 all_weights = [weight_1, weight_2, weight_3, weight_4]
-print(f'Weights: {all_weights}')
+print(f'Weights: {all_weights}\n')
+
+# Average of all weights will have two digits after the decimal, and be in a variable called avg_weight
+# Conversion specifier used to specify printing two digits after the decimal point
+avg_weight = (weight_1 + weight_2 + weight_3 + weight_4) / 4
+print('Average weight: {:.2f}'.format(avg_weight))
