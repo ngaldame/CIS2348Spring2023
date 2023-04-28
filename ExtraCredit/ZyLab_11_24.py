4d9c6b55f6ba107630b30915a37cb949a884db84
ngaldame <ngaldame@cougarnet.uh.edu>
2023-04-28 02:31:09 -0500

    Inputted sentences will be stored in 2 lists

--- ExtraCredit/ZyLab_11_24.py
+++ ExtraCredit/ZyLab_11_24.py
@@ -1,2 +1,7 @@
 # Nathan Galdamez Gomez, 2141118
 # This is ZyLab 11.24
+
+# Both sentences are taken as input that will be in two lists of strings
+sentence_1 = str(input()).split()
+sentence_2 = str(input()).split()
+
