4f350aa35ced21e60793adaf03438a4c20b60561
ngaldame <ngaldame@cougarnet.uh.edu>
2023-04-28 04:15:55 -0500

    Prompted 4 inputted numbers for weights in pounds

--- ExtraCredit/ZyLab_11_25.py
+++ ExtraCredit/ZyLab_11_25.py
@@ -1,2 +1,12 @@
 # Nathan Galdamez Gomez, 2141118
 # This is ZyLab 11.25
+
+# Prompt for four weights
+weight_1 = float(input('Enter weight 1:\n'))
+weight_2 = float(input('Enter weight 2:\n'))
+weight_3 = float(input('Enter weight 3:\n'))
+weight_4 = float(input('Enter weight 4:\n'))
+
+# All weights will be added to a list called all_weights, then will be outputted
+all_weights = [weight_1, weight_2, weight_3, weight_4]
+print(f'Weights: {all_weights}')
